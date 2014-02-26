526c1317c156e160b0b1a22ffdf9703d54aefa9e
leonco <leonco@126.com>
2014-02-26 10:27:44 +0800

    add func `ConvertToneNumber2ToneMark`

--- format.go
+++ format.go
@@ -1,1 +1,94 @@
 package pinyin4go
+
+import (
+	"regexp"
+	//"strconv"
+	//"bytes"
+	"fmt"
+	"strings"
+	"unicode/utf8"
+)
+
+const (
+	a        = 'a'
+	e        = 'e'
+	ou       = "ou"
+	unmarked = "aeiouv"
+	marked   = "āáăàaēéĕèeīíĭìiōóŏòoūúŭùuǖǘǚǜü"
+)
+
+func ConvertToneNumber2ToneMark(pinyin string) string {
+	lowerpy := strings.ToLower(pinyin)
+
+	// bad format
+	if ok, _ := regexp.MatchString(`[a-z]+[1-5]?`, lowerpy); !ok {
+		return lowerpy
+	}
+
+	// input string has no any tune number
+	// only replace v with ü (umlat) character
+	if ok, _ := regexp.MatchString(`[a-z]+[1-5]`, lowerpy); !ok {
+		return strings.Replace(lowerpy, "v", "ü", -1)
+	}
+
+	char := '$'
+	index := -1
+
+	unmarkedVowel := char
+	indexOfUnmarkedVowel := index
+
+	l := len(lowerpy)
+	tuneN := lowerpy[l-1] - '0'
+
+	indexOfA := strings.IndexByte(lowerpy, a)
+	indexOfE := strings.IndexByte(lowerpy, e)
+	indexOfOu := strings.Index(lowerpy, ou)
+
+	if indexOfA >= 0 {
+		unmarkedVowel = a
+		indexOfUnmarkedVowel = indexOfA
+	} else if indexOfE >= 0 {
+		unmarkedVowel = e
+		indexOfUnmarkedVowel = indexOfE
+	} else if indexOfOu >= 0 {
+		unmarkedVowel = rune(ou[0])
+		indexOfUnmarkedVowel = indexOfOu
+	} else {
+		for i := l - 1; i >= 0; i-- {
+			if strings.IndexByte(unmarked, lowerpy[i]) >= 0 {
+				indexOfUnmarkedVowel = i
+				unmarkedVowel = rune(lowerpy[i])
+				break
+			}
+		}
+	}
+	if char != unmarkedVowel && index != indexOfUnmarkedVowel {
+		row := strings.IndexRune(unmarked, unmarkedVowel)
+		col := tuneN - 1
+		fmt.Println(row*5 + int(col))
+		c := getMarked(row*5 + int(col))
+		var buf []byte
+		b1 := []byte(strings.Replace(lowerpy[:indexOfUnmarkedVowel], "v", "ü", -1))
+		b2 := []byte(lowerpy[indexOfUnmarkedVowel+1 : l-1])
+		buf = append(buf, b1...)
+		buf = append(buf, c...)
+		buf = append(buf, b2...)
+		return string(buf)
+	} else {
+		return lowerpy
+	}
+}
+
+func getMarked(i int) []byte {
+	b := []byte(marked)
+	j := 0
+	for len(b) > 0 {
+		_, n := utf8.DecodeRune(b)
+		if j == i {
+			return b[:n]
+		}
+		j++
+		b = b[n:]
+	}
+	return nil
+}
